2d67938b45c179ac30e363148d56ff7891673532
Ray Kim <rayk1224@yahoo.com>
2016-01-09 13:24:05 -0800

    edit is working and refixed server -- dont touch working code

--- Server/routes/Users.js
+++ Server/routes/Users.js
@@ -36,23 +36,23 @@ router.post('/admin', function (req, res){
 })
 
 router.put('/', function (req, res){
-  if (req.userId === req.body.id || isAdmin){
+  if (req.userId === req.body._id || req.isAdmin){
     User.findByIdAndUpdate(req.userId, req.body, function (err, updatedUser){
       res.status(err ? 400 : 200).send(err || updatedUser);
     })
-  
+  }else{
+    res.status(403).send('You are not authorized to do this action');
+  }
   // else if (req.isAdmin){
   //   console.log('admin')
   //   User.findByIdAndUpdate(req.body.id, req.body, function (err, updatedUser){
   //     res.status(err ? 400 : 200).send(err || updatedUser);
   //   })
   // }else{
-  }
-  res.status(403).send('You are not authorized to do this action');
 })
 
 router.delete('/', function (req, res){
-  if (req.userId === req.body.id){
+  if (req.userId === req.body._id){
     console.log('same');
     User.findByIdAndRemove(req.body.id, function (err, removedUser){
       res.status(err ? 400 : 200).send(err || 'removed');
--- Server/util/authMiddleware.js
+++ Server/util/authMiddleware.js
@@ -34,8 +34,8 @@ module.exports = function(req, res, next) {
     req.userId = decoded.id;
     User.findById(decoded.id, (err, user) => {
       if (err) return res.status(400).send('server error');
+      console.log(user.isAdmin);
       req.isAdmin = user.isAdmin;
-      console.log(user.isAdmin, req.isAdmin);
       next();
     })
   }
