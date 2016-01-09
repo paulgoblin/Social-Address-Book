541250b5451007bdd33e378259743331a9d74dc8
Paul Richter <mpr75@cornell.edu>
2016-01-08 18:44:32 -0800

    fix ui-route / API route conflicts

--- Client/app/services/login.js
+++ Client/app/services/login.js
@@ -6,12 +6,12 @@ function LoginSvc($http){
 
   this.register = function(input){
     console.log(input)
-    return $http.post('/auth/register', input)
+    return $http.post('/noHash/auth/register', input)
   }
 
   this.login = function(input){
     console.log(input.username)
-    return $http.post('/auth/login', input)
+    return $http.post('/noHash/auth/login', input)
   }
 
   this.setAuthHeader = function(token) {
