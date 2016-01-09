72d2cdba2b1c5aa567a2262c4461d0d6371871a4
Ray Kim <rayk1224@yahoo.com>
2016-01-08 18:48:42 -0800

    modal working on individual profile page

--- Client/app/components/landing_page/landing_page.js
+++ Client/app/components/landing_page/landing_page.js
@@ -1,8 +1,8 @@
 angular
   .module('app.landing_page', [])
-  .controller('LandingPageCtrl', ['LoginSvc', '$rootScope', '$state',LandingPageCtrl]);
+  .controller('LandingPageCtrl', ['LoginSvc', '$rootScope', '$state', 'StoreSvc', LandingPageCtrl]);
 
-function LandingPageCtrl(LoginSvc, $rootScope, $state){
+function LandingPageCtrl(LoginSvc, $rootScope, $state, StoreSvc){
   var landing_page = this;
   landing_page.showRegister = showRegister;
   landing_page.showLogin = showLogin;
@@ -30,8 +30,8 @@ function LandingPageCtrl(LoginSvc, $rootScope, $state){
   function loginResHandler(promise){
     promise.then(function(res){
       $rootScope.LS.set('token', `Bearer ${res.data.token}`)
-      $rootScope.LS.set('me', res.data.user)
       LoginSvc.setAuthHeader($rootScope.LS.get('token'));
+      StoreSvc.saveData('me', res.data.user);
       $state.go('home')
     }, function(err){
       landing_page.err_msg =  err.data;
