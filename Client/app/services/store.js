4efd0b0241ec4d9015ec45c1103c1a764fe4102d
Michael Richter <michaelrichter@michaels-mbp-2.attlocal.net>
2016-01-09 13:43:24 -0800

    front end updates favorites

--- Client/app/services/store.js
+++ Client/app/services/store.js
@@ -7,9 +7,21 @@ StoreSvc.$inject = []
 
 function StoreSvc(){
   let data = {};
+  // let listenerCbs = [];
+
+  // this.addChangeListener = function(cb) {
+  //   listenerCbs.push(cb);
+  // }
+
+  // var emitChange = function(){
+  //   listenerCbs.forEach(function(cb){
+  //     angular.isFunciton(cb)cb();
+  //   });
+  // }
 
   this.saveData = function(key, info){
     data[key] = info;
+    // emitChange();
   }
   this.returnData = function(key){
     return data[key];
